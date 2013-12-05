c9791bee814eaef1d686e3d0c382c6df0f7f2f2e
daanlouter <daan.louter@gmail.com>
2013-12-05 18:06:49 +0000

    added details for street

--- js/main.js
+++ js/main.js
@@ -1,37 +1,72 @@
-$(document).ready(function(){
+var diefstalArray = [];
+var totalLength;
+var rotterdamLength;
+var incidentenRotterdam = [];
+var incidentenStraat = [];
+var straatNaam;
 
+function getDetailsPerStreet(incidentenStraat){
+	var lijstmetBuurten = [];
+	var lijstmetTijden = [];
+	console.log(incidentenStraat[0]);
+	var x =0;
 
-$('#straatForm').on("submit", function(){
-	event.preventDefault();
-	console.log('hoi');
-	var straatNaam = $('.streetField').val();
-	var incidentenRotterdam = [];
-	var incidentenStraat = [];
-	console.log(straatNaam);
-	console.log()
-	$.getJSON( "js/data.json", function( data ) {
-		var totalLength = data.length;
+	$.each(incidentenStraat, function(i,j){
+		var huidigDagdeel = j['Gemiddelde dagdeel (6 uren)'];
+		var posBuurt = lijstmetBuurten.map(function(e) { return e.naam; }).indexOf(j.Buurt);
+		var posTijden = lijstmetTijden.map(function(e) { return e.dagdeel; }).indexOf(huidigDagdeel);
 		
+		if(posBuurt == -1){
+			lijstmetBuurten.push({naam: j.Buurt, aantal:1 });
+		}else{
+			lijstmetBuurten[posBuurt].aantal = lijstmetBuurten[posBuurt].aantal + 1;
+		}
 
-		$.each(data, function(i,a){
-			if(a.Plaats == "ROTTERDAM"){
-				incidentenRotterdam.push(a);
-			}
-			
-			if(a.Straat == straatNaam.toUpperCase()){
+		if(posTijden == -1){
+			lijstmetTijden.push({dagdeel: huidigDagdeel, aantal:1 });
+		}else{
+			lijstmetTijden[posTijden].aantal = lijstmetTijden[posTijden].aantal + 1;
+		}
+		
+	});
 
-				incidentenStraat.push(a);
+	$.each(lijstmetTijden, function(index, val) {
+		 $('.wrapper').append('<p>Er zijn ' + val.aantal + ' diefstallen om ' + val.dagdeel + ' in de ' +straatNaam+ '</p>');
+	});
+	
+	// $('.wrapper').append('<p>' +incidentenStraat.length+ ' diefstallen in ' +straatNaam+ '</p>');
+}
+
+
+
+$(document).ready(function(){
+	$.getJSON( "js/data.json", function( data ) {
+		$.each(data, function(i,j){
+			diefstalArray.push(j);
+			if(j.Plaats == "ROTTERDAM"){
+				incidentenRotterdam.push(j);
 			}
 		});
-		var rotterdamLength = incidentenRotterdam.length;
-
-		var percentage = Math.round((rotterdamLength/totalLength)*100);
-
-		$('.wrapper').html('<p>' +totalLength+ ' diefstallen in totaal</p>');
-		$('.wrapper').append('<p>' +rotterdamLength+ ' diefstallen in Rotterdam</p>');
-		$('.wrapper').append('<p>' +percentage+ ' % in Rotterdam</p>');
-		$('.wrapper').append('<p>' +incidentenStraat.length+ ' diefstallen in ' +straatNaam+ '</p>');
-		console.log(data[0], totalLength,rotterdamLength)
+		totalLength = diefstalArray.length;
+		rotterdamLength = incidentenRotterdam.length;
 	});
 });
+
+$('#straatForm').on("submit", function(){
+	var incidentenStraat = [];
+	event.preventDefault();
+	straatNaam = $('.streetField').val();
+	// straatNaam = "Heemraadssingel"
+	$.each(diefstalArray, function(i,a){
+		//tel incidenten in jouw straat
+		if(a.Straat == straatNaam.toUpperCase()){
+			incidentenStraat.push(a);
+		}
+	});
+
+	
+	$('.wrapper').html('<p>' +totalLength+ ' diefstallen in totaal</p>');
+	$('.wrapper').append('<p>' +rotterdamLength+ ' diefstallen in Rotterdam</p>');
+	$('.wrapper').append('<p>' +incidentenStraat.length+ ' diefstallen in ' +straatNaam+ '</p>');
+	getDetailsPerStreet(incidentenStraat);
 });
